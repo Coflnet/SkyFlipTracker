c693e1b7bc864ac11ca331de9286a1339117ee08
Äkwav <xekwavx@gmail.com>
2021-12-31 22:21:04 +0100

    use correct enum types

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Reflection;
+using System.Text.Json.Serialization;
 using Coflnet.Sky.SkyAuctionTracker.Models;
 using Coflnet.Sky.SkyAuctionTracker.Services;
 using hypixel;
@@ -33,7 +34,8 @@ namespace Coflnet.Sky.SkyAuctionTracker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddJsonOptions(options =>
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "SkyTracker", Version = "v1" });
