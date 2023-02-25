49c13cca02078897f3a36f94c6cae547dc164c7f
Äkwav <ekwav@coflnet.com>
2023-02-25 20:39:22 +0100

    up sell consume rate

--- Services/TrackerBackgroundService.cs
+++ Services/TrackerBackgroundService.cs
@@ -109,7 +109,7 @@ namespace Coflnet.Sky.SkyAuctionTracker.Services
                         logger.LogError(e, "could not save event once");
                         await Task.Delay(1000);
                     }
-            }, stoppingToken, "fliptracker", 16);
+            }, stoppingToken, "fliptracker", 60);
             throw new Exception("consuming sells stopped");
         }
         private async Task LoadFlip(CancellationToken stoppingToken)
