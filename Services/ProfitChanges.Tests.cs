6e00ab436b6e2b7219a56d0b8de41471d79c4389
Äkwav <ekwav@coflnet.com>
2023-05-29 12:44:19 +0200

    move trackerservice tests

--- Services/ProfitChanges.Tests.cs
+++ Services/ProfitChanges.Tests.cs
@@ -787,25 +787,3 @@ public class ProfitChangeTests
         return all;
     }
 }
-
-public class TrackerServiceTests
-{
-    [Test]
-    public void CheckLevelDisplay()
-    {
-        var buy = new ApiSaveAuction()
-        {
-            ItemName = "[Lvl 30] Bat",
-            FlatenedNBT = new() { { "exp", "500000" } },
-            Tag = "PET_BAT"
-        };
-        var sell = new ApiSaveAuction()
-        {
-            ItemName = "[Lvl 60] Bat",
-            FlatenedNBT = new() { { "exp", "1000000.1" } },
-            Tag = "PET_BAT"
-        };
-        var name = TrackerService.GetDisplayName(buy, sell);
-        Assert.AreEqual("[Lvl 30->60] Bat", name);
-    }
-}
