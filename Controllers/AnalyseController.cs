47565e451b72cb9a5aad348ca7626ab82d5e6536
Äkwav <ekwav@coflnet.com>
2024-05-28 15:48:20 +0200

    extend da list

--- Controllers/AnalyseController.cs
+++ Controllers/AnalyseController.cs
@@ -45,6 +45,8 @@ namespace Coflnet.Sky.SkyAuctionTracker.Controllers
             "407fcb6e116245c8b3284f733a931c5b", "f3f10d95a1b544e3b5df88fbfcae5fba", "ddb96c0938cd4e498ed57b2814b3d9b7",
             "c9bf10d8f394436283f3718d12c6950b", "cd1b67e5ce8c4dfcabcf74c6afe2478c", "c0b2620403b943309f6f98d164bb7249", //  https://discord.com/channels/267680588666896385/1244436738437550142/1244569273574101054
             "95cc34fe0fd8438592a7a92c63961838", "3637befb840c4b138ab2f16fa7e5e3f1", "d29eaf93c0fb4657a03583825c11d62b", // _/
+            "0bddfd9d0c6d4f2d9fb9e1319cf468f1", 
+            "d223be875ed04d72b237789bd92b04d2", "877e47356af746a0ad638a4cdb0d4249", // floored?!?
             "d472ab290c0f4cbbaccefdce90176d32" // See https://discord.com/channels/267680588666896385/1006897388641853470/1011757951087820911
         };
         public static HashSet<string> BadPlayersList => BadPlayers;
