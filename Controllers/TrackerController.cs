22a8de149bfe24ec714b54554eeca6d505f7415e
Äkwav <ekwav@coflnet.com>
2024-08-12 14:58:35 +0200

    allow get all

--- Controllers/TrackerController.cs
+++ Controllers/TrackerController.cs
@@ -210,13 +210,16 @@ namespace Coflnet.Sky.SkyAuctionTracker.Controllers
         [HttpGet]
         [Route("/flips/{PlayerId}")]
         [ResponseCache(Duration = 60, Location = ResponseCacheLocation.Any, VaryByQueryKeys = new[] { "from", "to" })]
-        public async Task<IEnumerable<PastFlip>> GetFlipsOfPlayer(Guid PlayerId, DateTime from, DateTime to)
+        public async Task<IEnumerable<PastFlip>> GetFlipsOfPlayer(Guid PlayerId, DateTime from, DateTime to, bool getAll = false)
         {
             if (from == DateTime.MinValue)
                 from = DateTime.Now.AddYears(-1);
             if (to == DateTime.MinValue)
                 to = DateTime.Now;
-            return (await flipStorageService.GetFlips(PlayerId, from, to)).GroupBy(f=>f.Uid).Select(f=>f.First());
+            var all = await flipStorageService.GetFlips(PlayerId, from, to);
+            if (getAll)
+                return all;
+            return all.GroupBy(f=>f.SellAuctionId).Select(f=>f.First());
         }
 
         [HttpGet]
